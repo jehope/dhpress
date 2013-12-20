6bb08028b4908cf005358c879fa64ad3cbe97b1a
jehope <jehope2@gmail.com>
2013-12-20 11:47:21 -0500

    Fixed the term creation process by cleaning data prior to searching
    Didnt need the get_term

--- trunk/php/dhp-project-functions.php
+++ trunk/php/dhp-project-functions.php
@@ -1396,7 +1396,7 @@ function dhpSaveProjectSettings()
 	update_post_meta($dhp_projectID, 'project_settings', $settings);
 
 		// Ajax call must terminate with "die"
-	die('working... '. $settings);
+	die('saving... '. $settings);
 } // dhpSaveProjectSettings()
 
 
@@ -1415,19 +1415,24 @@ function dhpUpdateTaxonomy($mArray, $mote_name, $dhp_tax_name)
 	$parent_id = $parent_term->term_id;
 	$args = array('parent' => $parent_id);
 
-
+	$updateTaxObject['debug']['tax'] = $dhp_tax_name;
+	$updateTaxObject['debug']['parent'] = $args;
+	$updateTaxObject['debug']['mArrayLoop'] = array();
 	//loop through array and create terms with parent(mote_name)
-
-	foreach ($mArray as &$value) {
-   		$termIs = term_exists( $value, $dhp_tax_name, $parent_id ); 	
-   		
+	
+	foreach ($mArray as $value) {
+		//escape value and search
+   		$termIs = term_exists( addslashes($value), $dhp_tax_name, $parent_id );
+   		//debug
+   		// array_push($updateTaxObject['debug']['mArrayLoop'], addslashes($value));
+   		// array_push($updateTaxObject['debug']['mArrayLoop'], $termIs);
    		if(!$termIs) {
+   			//if term doesn't exist, create
    			wp_insert_term( $value, $dhp_tax_name, $args );
    		}
    		else {
-   			$termName = get_term($termIs->term_id, $dhp_tax_name);
-   			// $args = array('parent' => $parent_id, 'alias_of'=>$termName->slug);
-   			wp_update_term( $value, $dhp_tax_name, $args );
+   			//update term using id
+   			wp_update_term( $termIs->term_id, $dhp_tax_name, $args );
    		}
 	}
 
@@ -1460,8 +1465,12 @@ function dhpUpdateTaxonomy($mArray, $mote_name, $dhp_tax_name)
 			$term ->icon_url = $term_url;
 		}
 	}
+	//testing return
+	// $updateTaxObject['debug'] = 'test';
+	$updateTaxObject['terms'] = $terms_loaded;
+	return $updateTaxObject;
 
-	return $terms_loaded;
+	//return $terms_loaded;
 } // dhpUpdateTaxonomy()
 
 
@@ -1534,11 +1543,13 @@ function dhpCreateLegendTax()
 	//returns unique array of values
 	$mArray = createMoteValueArrays($custom_field,$mote_delim,$dhp_projectID);
 	//create/update terms with mArray
-	$terms_loaded = dhpUpdateTaxonomy($mArray, $mote['name'], $dhp_tax_name);
+	$legendObject = dhpUpdateTaxonomy($mArray, $mote['name'], $dhp_tax_name);
 
+	//testing returns
 	//die(json_encode($mArray));
-	die(json_encode($terms_loaded));
-	//die(json_encode($exclude_string));
+	die(json_encode($legendObject));
+
+	// die(json_encode($legendObject['terms']));
 } // dhpCreateLegendTax()
 
 
