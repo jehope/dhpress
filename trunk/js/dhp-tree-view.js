8e50564d1036f92f998a18da3a5bb5d78ea3e2e9
gaelicmichael <gaelicmichael@yahoo.com>
2014-07-28 14:19:34 -0400

    Radial Tree working

--- trunk/js/dhp-tree-view.js
+++ trunk/js/dhp-tree-view.js
@@ -18,13 +18,19 @@ var dhpTreeView = {
         //                  legendTerms = terms array of Legend
 
         //                  iWidth, iHeight = pixel width and height of "palette"
-        //                  padding
-        //                  svg = svg div
+        //                  padding = padding in pixels -- tree-specific
+        //                      flat: the space given to labels of first and last nodes
+        //                  svg = the D3 object for the SVG div
+        //                  vis = the D3 object which takes the shapes
 
-        //                  radius = radius of geometric markers
+        //                  mRadius = radius of geometric markers
         //                  iconSize = "s" | "m" | "l"
         //                  icons (Object with properties that are SNAP path defs)
         //                      ballon, magGlass, thumbtack
+        //                  tRadius = radius of radial tree
+
+        //                  m0 = for rotating radial tree
+        //                  rotate = degree to which radial tree currently rotated
 
         //                  === D3 Objects ===
         //                  cluster = cluster layout
@@ -62,7 +68,7 @@ var dhpTreeView = {
         dhpTreeView.iWidth  = typeof(treeEP.width)  === 'number' ? treeEP.width  : parseInt(treeEP.width);
         dhpTreeView.iHeight = typeof(treeEP.height) === 'number' ? treeEP.height : parseInt(treeEP.height);
         dhpTreeView.padding = typeof(treeEP.padding) === 'number' ? treeEP.padding  : parseInt(treeEP.padding);
-        dhpTreeView.radius  = typeof(treeEP.radius) === 'number' ? treeEP.radius  : parseInt(treeEP.radius);
+        dhpTreeView.mRadius = typeof(treeEP.radius) === 'number' ? treeEP.radius  : parseInt(treeEP.radius);
         dhpTreeView.fSize   = typeof(treeEP.fSize) === 'number' ? treeEP.fSize  : parseInt(treeEP.fSize);
             // set view/scroll window parameters
 
@@ -80,6 +86,8 @@ var dhpTreeView = {
             // Create placeholder for Legend menu
         jQuery('#dhp-controls').append(Handlebars.compile(jQuery("#dhp-script-tree-legend-head").html()));
 
+        jQuery(document).foundation();
+
             // Create SVG for D3
         jQuery("#dhp-visual").append('<div id="svg-container"></div>');
         dhpTreeView.svg = d3.select("#svg-container").append("svg").attr("width", dhpTreeView.iWidth).attr("height", dhpTreeView.iHeight);
@@ -87,10 +95,37 @@ var dhpTreeView = {
             // Other initialization will depend on form of tree
         switch (treeEP.form) {
         case 'flat':
-            dhpTreeView.svg = dhpTreeView.svg.append("g").attr("transform", "translate("+treeEP.padding+",0)");
+            dhpTreeView.vis = dhpTreeView.svg.append("g").attr("transform", "translate("+treeEP.padding+",0)");
 
-            dhpTreeView.cluster = d3.layout.cluster().size([dhpTreeView.iHeight, dhpTreeView.iWidth - (treeEP.padding*2)]);
+            dhpTreeView.tree = d3.layout.cluster().size([dhpTreeView.iHeight, dhpTreeView.iWidth - (treeEP.padding*2)]);
             dhpTreeView.diagonal = d3.svg.diagonal().projection(function(d) { return [d.y, d.x]; });
+            break;
+        case 'radial':
+            dhpTreeView.tRadius = Math.min(dhpTreeView.iWidth, dhpTreeView.iHeight) / 2;
+            dhpTreeView.m0 = null;
+            dhpTreeView.rotate = 0;
+
+                // Size here is in a radial coordinate system
+                // Leave a pixel padding around outside arc
+            dhpTreeView.tree = d3.layout.tree().size([360, dhpTreeView.tRadius - (dhpTreeView.padding + 2)])
+                    // set the spacing function between neighboring nodes
+                .separation(function(a, b) { return (a.parent == b.parent ? 1 : 2) / a.depth; });
+
+                // Generator of coordinates based on "diagonal" space
+                // X is the angle and Y is the position along radius
+            dhpTreeView.diagonal = d3.svg.diagonal.radial()
+                .projection(function(d) { return [d.y, d.x / 180 * Math.PI]; });
+
+            dhpTreeView.vis = dhpTreeView.svg.append("g").attr("transform", "translate(" + (dhpTreeView.tRadius+1)
+                                    + "," + (dhpTreeView.tRadius+1) + ")");
+
+                // Create the outer rotate wheel
+            dhpTreeView.vis.append("path")
+                .attr("class", "arc")
+                .attr("d", d3.svg.arc().innerRadius(dhpTreeView.tRadius - dhpTreeView.padding).outerRadius(dhpTreeView.tRadius).startAngle(0).endAngle(2 * Math.PI))
+                .on("mousedown.spin", dhpTreeView.mDownSpin)
+                .on("mousemove.spin", dhpTreeView.mMoveSpin)
+                .on("mouseup.spin", dhpTreeView.mUpSpin);
             break;
         }
 
@@ -105,6 +140,7 @@ var dhpTreeView = {
             },
             success: function(data, textStatus, XMLHttpRequest)
             {
+                    // Prepare the data for use
                 dhpTreeView.rawData = JSON.parse(data);
                 dhpTreeView.legendTerms = dhpTreeView.rawData[0];
                 if (dhpTreeView.legendTerms.type !== 'filter') {
@@ -130,23 +166,23 @@ var dhpTreeView = {
         var newRowHeight, checkboxMargin;
 
             //resize legend term position for long titles
-        // jQuery('.active-legend .terms').css({top: jQuery('.active-legend .legend-title').height() });
+        jQuery('.active-legend .terms').css({top: jQuery('.active-legend .legend-title').height() });
 
-        //     //resize legend items that are two lines and center checkbox
-        // jQuery('.active-legend .row').each(function(key,value) {
-        //         //height of row containing text(could be multiple lines)
-        //     newRowHeight   = jQuery('.columns', this).eq(1).height();
-        //         // variable to center checkbox in row
-        //     checkboxMargin = (newRowHeight - dhpTreeView.checkboxHeight) / 2;
-        //         // set elements in rows with new values
-        //     jQuery('.columns', this).eq(0).height(newRowHeight);
-        //     jQuery('.columns', this).eq(0).find('input').css({'margin-top': checkboxMargin});
-        // });
+            //resize legend items that are two lines and center checkbox
+        jQuery('.active-legend .row').each(function(key,value) {
+                //height of row containing text(could be multiple lines)
+            newRowHeight   = jQuery('.columns', this).eq(1).height();
+                // variable to center checkbox in row
+            checkboxMargin = (newRowHeight - dhpTreeView.checkboxHeight) / 2;
+                // set elements in rows with new values
+            jQuery('.columns', this).eq(0).height(newRowHeight);
+            jQuery('.columns', this).eq(0).find('input').css({'margin-top': checkboxMargin});
+        });
 
             // Width of svg-container is same as visual space
         jQuery('#svg-container').width(jQuery('#dhp-visual').width()-2);
             // Height of svg-container will be total viz space minus height of navbar, margins, border & scroll bar itself
-        var svgHeight = jQuery('#dhp-visual').height() - (dhpTreeView.controlHeight+40);
+        var svgHeight = jQuery('#dhp-visual').height() - (dhpTreeView.controlHeight+10);
         jQuery('#svg-container').height(svgHeight);
     }, // dhpUpdateSize()
 
@@ -218,7 +254,6 @@ var dhpTreeView = {
     }, // createLegend()
 
 
-
         // PURPOSE: To determine color to use for marker
         // INPUT:   featureVals = array of category IDs (integers) associated with a feature/marker
         // RETURNS: Partial string to set color and background color (w/o closing ")
@@ -271,41 +306,146 @@ var dhpTreeView = {
         //                    [n] = Feature Tree
     createGraph: function()
     {
-        var tree = dhpTreeView.rawData[dhpTreeView.rawData.length-1];
+        var nodeData = dhpTreeView.rawData[dhpTreeView.rawData.length-1];
+        var padding = dhpTreeView.treeEP.padding;
+        var nodes, links, link, node;
 
-        var xStart = dhpTreeView.treeEP.padding;
+        switch (dhpTreeView.treeEP.form) {
+        case 'flat': 
+                // The cluser algorithms will create placement in x and y fields
+            nodes = dhpTreeView.tree.nodes(nodeData);
+            links = dhpTreeView.tree.links(nodes);
 
-            // The cluser algorithms will create placement in x and y fields
-        var nodes = dhpTreeView.cluster.nodes(tree),
-            links = dhpTreeView.cluster.links(nodes);
+                // Create branches ("links") between markers ("nodes")
+            link = dhpTreeView.vis.selectAll(".link").data(links)
+                        .enter().append("path")
+                        .attr("class", "link")
+                        .attr("d", dhpTreeView.diagonal);
 
-            // Create branches ("links") between markers ("nodes")
-        var link = dhpTreeView.svg.selectAll(".link").data(links)
+                // Create the markers ("nodes") themselves
+            node = dhpTreeView.vis.selectAll(".node").data(nodes)
+                        .enter().append("g")
+                        .attr("class", "node")
+                        .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; })
+                        .on("click", function(d, index) {
+                            console.log("Selected node name "+d.name+" which is index "+index);
+                        });
+
+            node.append("circle")
+                  .attr("r", dhpTreeView.mRadius)
+                  .style("fill", function(d) {
+                      return dhpTreeView.getItemColor(d.properties.categories);
+                  });
+
+            node.append("text")
+                  .attr("dx", function(d) { return d.children ? -8 : 8; })
+                  .attr("dy", 3)
+                  .style("font-size", dhpTreeView.fSize+'px')
+                  .style("text-anchor", function(d) { return d.children ? "end" : "start"; })
+                  .text(function(d) { return d.name; });
+            break;
+
+        case 'radial':
+            nodes = dhpTreeView.tree.nodes(nodeData);
+            links = dhpTreeView.tree.links(nodes);
+
+              // Create all of the link paths (using diagonal projection)
+            link = dhpTreeView.vis.selectAll(".link")
+                    .data(links)
                     .enter().append("path")
                     .attr("class", "link")
                     .attr("d", dhpTreeView.diagonal);
 
-            // Create the markers ("nodes") themselves
-        var node = dhpTreeView.svg.selectAll(".node").data(nodes)
-                    .enter().append("g")
-                    .attr("class", "node")
-                    .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; })
-                    .on("click", function(d, index) {
-                        console.log("Selected node name "+d.name+" which is index "+index);
-                    });
+              // Create all of the g-elements that contain node svg-elements
+            node = dhpTreeView.vis.selectAll(".node")
+                .data(nodes)
+                .enter()
+                .append("g")
+                .attr("class", "node")
+                .attr("transform", function(d) { return "rotate(" + (d.x - 90) + ")translate(" + d.y + ")"; })
+                .on("click", function(d, index) {
+                    console.log("Selected node name "+d.name+" which is index "+index);
+                });
 
-        node.append("circle")
-              .attr("r", dhpTreeView.radius)
-              .style("fill", function(d, index) {
-                  return dhpTreeView.getItemColor(d.properties.categories);
-              });
+                // Create the actual circle element
+            node.append("circle")
+                .attr("r", dhpTreeView.mRadius)
+                    // In actuality, will need to access property of d
+                .style("fill", function(d) {
+                    return dhpTreeView.getItemColor(d.properties.categories);
+                })
+                // Create the text label for the node
+            node.append("text")
+                .style( "font-size", dhpTreeView.fSize+'px')
+                    // 1/3 character size between anchor point (of g) and text
+                .attr("dy", ".31em")
+                    // Whether the end or beginning of the label is next to the node depends on the angle
+                .attr("text-anchor", function(d) { return d.x < 180 ? "start" : "end"; })
+                    // May also need to turn text upside down
+                .attr("transform", function(d) { return d.x < 180 ? "translate(8)" : "rotate(180)translate(-8)"; })
+                .text(function(d) { return d.name; });
+            break;
+        }
+    }, // createData()
 
-        node.append("text")
-              .attr("dx", function(d) { return d.children ? -8 : 8; })
-              .attr("dy", 3)
-              .style("font-size", dhpTreeView.fSize+'px')
-              .style("text-anchor", function(d) { return d.children ? "end" : "start"; })
-              .text(function(d) { return d.name; });
 
-    } // createData()
-}
+// MOUSE CONTROL CODE ==========================
+
+    mTrans: function(e) {
+      return [e.pageX - dhpTreeView.tRadius, e.pageY - dhpTreeView.tRadius];
+    }, // transMouse()
+
+    mDownSpin: function() {
+      dhpTreeView.m0 = dhpTreeView.mTrans(d3.event);
+      d3.event.preventDefault();
+    }, // mDownSpin()
+
+    mMoveSpin: function() {
+      if (dhpTreeView.m0) {
+        var m1 = dhpTreeView.mTrans(d3.event),
+            dm = Math.atan2(dhpTreeView.mCross(dhpTreeView.m0, m1), dhpTreeView.mDot(dhpTreeView.m0, m1)) * 180 / Math.PI,
+            tx = "rotate3d(0,0,0," + dm + "deg)";
+            // tx = "translate3d(0," + (ry - rx) + "px,0)rotate3d(0,0,0," + dm + "deg)translate3d(0," + (rx - ry) + "px,0)";
+        dhpTreeView.svg
+            .style("-moz-transform", tx)
+            .style("-ms-transform", tx)
+            .style("-webkit-transform", tx);
+      }
+    }, // mMoveSpin()
+
+    mUpSpin: function() {
+      if (dhpTreeView.m0) {
+        var m1 = dhpTreeView.mTrans(d3.event),
+            dm = Math.atan2(dhpTreeView.mCross(dhpTreeView.m0, m1), dhpTreeView.mDot(dhpTreeView.m0, m1)) * 180 / Math.PI,
+            tx = "rotate3d(0,0,0,0deg)";
+
+        dhpTreeView.rotate += dm;
+        if (dhpTreeView.rotate > 360) { dhpTreeView.rotate -= 360; }
+        else if (dhpTreeView.rotate < 0) { dhpTreeView.rotate += 360; }
+        dhpTreeView.m0 = null;
+
+        dhpTreeView.svg
+            .style("-moz-transform", tx)
+            .style("-ms-transform", tx)
+            .style("-webkit-transform", tx);
+
+        dhpTreeView.vis
+            .attr("transform", "translate(" + dhpTreeView.tRadius + "," + dhpTreeView.tRadius +
+                    ")rotate(" + dhpTreeView.rotate + ")")
+            // .attr("transform", "translate(" + rx + "," + ry + ")rotate(" + rotate + ")")
+          .selectAll("g.node text")
+            .attr("dx", function(d) { return (d.x + dhpTreeView.rotate) % 360 < 180 ? 8 : -8; })
+            .attr("text-anchor", function(d) { return (d.x + dhpTreeView.rotate) % 360 < 180 ? "start" : "end"; })
+            .attr("transform", function(d) { return (d.x + dhpTreeView.rotate) % 360 < 180 ? null : "rotate(180)"; });
+      }
+    }, // mUpSpin()
+
+    mCross: function(a, b) {
+      return a[0] * b[1] - a[1] * b[0];
+    }, // mCross()
+
+    mDot: function(a, b) {
+      return a[0] * b[0] + a[1] * b[1];
+    } // mDot()
+
+} //  dhpTreeView
